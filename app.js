9b5d0f1ba82911c260821814ed6696bd01f66e3d
Andrew Harasymiw <aharasymiw@gmail.com>
2024-05-26 00:48:46 -0700

    backing up

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/poll', (req, res) => {
+
+})
+
+app.post('/poll', (req, res) => {
+
+})
+
+app.get('/poll/invite', (req, res) => {
+    res.sendFile(path.join(__dirname, 'poll_invite.png'));
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
 })
 
 app.listen(PORT, () => {
